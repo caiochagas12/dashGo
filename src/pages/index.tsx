c88bfc0d3a5c8268ab935a7c31a45480096e695a
Caio Vinicius <caiochagas77@hotmail.com>
2021-06-29 11:39:26 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,42 @@
-export default function Home() {
+import { Flex , Button , Stack } from '@chakra-ui/react'
+import  Head  from 'next/head';
+import { Input } from '../components/Form/Input'
+ 
+export default function SignIn() {
+
   return (
-    <h1>Fica frio ai</h1>
+<>
+  <Head> <title>Sign in | DashGO.</title> </Head>
+    <Flex 
+     w='100vw'
+     h='100vh' 
+     align='center' 
+     justify='center'
+    >
+
+    <Flex as='form' 
+     w='100%' 
+     maxWidth={360}
+     bg='gray.800' 
+     p='8' 
+     borderRadius={8}
+     flexDirection='column'
+    >
+
+        <Stack spacing='4'>
+
+ <Input type='email' name='email' label='E-mail'/>       
+ <Input type='password' name='password' label='Senha'/>    
+
+      </Stack>
+
+       <Button type='submit' mt='6' size='lg'colorScheme='pink'>Entrar</Button>
+    
+
+    </Flex>
+
+    </Flex>
+</>
   );
+
 }
