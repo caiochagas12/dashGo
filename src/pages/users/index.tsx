6d58ca7862a72659f615c62a2129897360ece87f
Caio Vinicius <caiochagas77@hotmail.com>
2021-07-22 18:35:56 -0300

    add axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,12 +6,12 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
+import { api } from '../../services/api'
 
 export default function UserList(){
 
-    const { data, isLoading, error } = useQuery('users', async () => {
-        const response = await fetch('http://localhost:3000/api/users')
-        const data = await response.json()
+    const { data, isLoading, error, isFetching } = useQuery('users', async () => {
+    const { data } = await api.get('users')
 
         const users = data.users.map(user => {
             return {
@@ -46,7 +46,10 @@ export default function UserList(){
         
              <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
                  <Flex mb='8' justify='space-between' align='center'>
-                     <Heading size='lg' fontWeight='normal'>Usuários</Heading>
+                     <Heading size='lg' fontWeight='normal'>
+                         Usuários
+                         { !isLoading && isFetching && <Spinner size='sm' color='gray.500' ml='4'/>}
+                         </Heading>
                      
             <Link href={'users/create'} passHref>
                      <Button as='a' 
